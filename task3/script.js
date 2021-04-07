ebae1f58bd0e9f0bd56616608066fbca1edb6101
ONeal182 <kigat63@gmail.com>
2021-04-07 18:21:16 +0300

    done task3 ++

--- task3/script.js
+++ task3/script.js
@@ -1,19 +1,28 @@
-const trafficLightEl = document.querySelector('#trafficLight');
+const trafficLightEl = document.querySelectorAll('#trafficLight');
+const trafficLightElFirst = document.querySelector('[data-numb = first]');
+const trafficLightElSecond = document.querySelector('[data-numb = second]');
+const trafficLightElThirnd = document.querySelector('[data-numb = third]');
+trafficLightEl.forEach(trafficLightEl => trafficLightEl.addEventListener('click', function(){
 
+    makeRed();
 function makeRed(){
-    trafficLightEl.style.background = ('red');
+    trafficLightElThirnd.style.background = ('black');
+    trafficLightElFirst.style.background = ('red');
     trafficLightEl.removeEventListener('click', makeRed);
     trafficLightEl.addEventListener('click', makeYellow);
 }
 function makeYellow(){
-    trafficLightEl.style.background = ('Yellow');
+    trafficLightElSecond.style.background = ('Yellow');
+    trafficLightElFirst.style.background = ('black');
     trafficLightEl.removeEventListener('click', makeYellow);
     trafficLightEl.addEventListener('click', makeGreen);
 }
 function makeGreen() {
-    trafficLightEl.style.background = ('green');
+    trafficLightElSecond.style.background = ('black');
+    trafficLightElThirnd.style.background = ('green');
+
     trafficLightEl.removeEventListener('click', makeGreen);
     trafficLightEl.addEventListener('click', makeRed);
 }
 
-trafficLightEl.addEventListener('click', makeRed);
+}))
